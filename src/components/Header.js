059b563daaadc8d18593053e3bbd416fb3d39190
Benin S <thorben21dash@gmail.com>
2023-11-17 12:36:18 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,56 @@
+import "../App.css";
+import logo from "../images/travel_company_logo.png";
+function Header() {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-lg">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img
+              src={logo}
+              alt="logo"
+              className="logo"
+              style={{ width: "8rem", height: "4rem" }}
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link fw-bold active"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link fw-bold" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link fw-bold" href="#">
+                  Tours
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
